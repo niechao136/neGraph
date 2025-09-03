6a47968a1aba0e3b0bf8e8cfec173ebbeb4197c9
nie.chao <nie.chao@advantech.com.cn>
2025-09-03 17:56:16 +0800

    前端加入 GZIP 打包

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
+const CompressionPlugin = require("compression-webpack-plugin")
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new CompressionPlugin({
+        test: /\.js$|\.html$|\.css$|\.jpe?g$|\.png$|\.svg$|\.webp$|gif$|\.xlsx$|\.ttf$|\.woff$|\.woff2/,
+        threshold: 10 * 1024,
+        deleteOriginalAssets: false,
+      }),
+    ],
+  },
 })
